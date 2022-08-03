03e34d9ae6618b5aa74bc07e41e65cebf2195ac6
HelioFraga <heliofraganeto01@gmail.com>
2022-08-03 17:35:16 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -3,6 +3,22 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = 'python'
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = (input('Qual a Letra?'))
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper == letra.upper()):
+                print('Encontrei a letra {} na posição {}'.format(letra, index))
+            index = index + 1
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
